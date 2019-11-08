836ef925cee21da901bb4eec89c870f515a6786d
João Justo <joaovjusto03>
2019-11-07 21:53:39 -0300

    Fix

--- spec/MathUtils.js
+++ spec/MathUtils.js
@@ -1,53 +1,75 @@
-describe("MathUtils", function() {
+describe("Todos", function() {
     var calc;
  
     //This will be called before running each spec
     beforeEach(function() {
         calc = new MathUtils();
         spyOn(calc,'sum')
-        spyOn(calc,'first')
+        // spyOn(calc,'first')
+        spyOn(calc,'second')
+        spyOn(window.console, 'log');
     });
  
-    describe("when calc is used to peform basic math operations", function(){
+    describe("Exercícios Testes", function(){
          
         // Spec for sum operation
-        it("should be able to calculate sum of 3 and 5", function() {
-             //call any method
-             calc.sum(3,5);
-            // verify it got executed
-            expect(calc.sum).toHaveBeenCalled();
-            expect(calc.sum).toHaveBeenCalledWith(3,5);
-        });
+        // it("should be able to calculate sum of 3 and 5", function() {
+        //      //call any method
+        //      calc.sum(3,5);
+        //     // verify it got executed
+        //     expect(calc.sum).toHaveBeenCalled();
+        //     expect(calc.sum).toHaveBeenCalledWith(3,5);
+        // });
         
-        // Spec for minus operation
-        it("should be able to calculate minus of 10 and 5",function(){
-            expect(calc.minus(10,5)).toEqual(5);
-        });
+        // // Spec for minus operation
+        // it("should be able to calculate minus of 10 and 5",function(){
+        //     expect(calc.minus(10,5)).toEqual(5);
+        // });
  
-        // Spec for multiply operation
-        it("should be able to multiply 10 and 40", function() {
-            expect(calc.multiply(10, 40)).toEqual(400);
-        });
+        // // Spec for multiply operation
+        // it("should be able to multiply 10 and 40", function() {
+        //     expect(calc.multiply(10, 40)).toEqual(400);
+        // });
  
-        // Spec for factorial operation for positive number
-        it("should be able to calculate factorial of 9", function() {
-            expect(calc.factorial(9)).toEqual(362880);
-        });
+        // // Spec for factorial operation for positive number
+        // it("should be able to calculate factorial of 9", function() {
+        //     expect(calc.factorial(9)).toEqual(362880);
+        // });
          
-        // Spec for factorial operation for negative number
-        it("should be able to throw error in factorial operation when the number is negative", function() {
+        // // Spec for factorial operation for negative number
+        // it("should be able to throw error in factorial operation when the number is negative", function() {
+        //     expect(function() {
+        //         calc.factorial(-7)
+        //     }).toThrowError(Error);
+        // });
+
+        it("Should throw an error", function() {
+            //call any method
+        //     calc.first(3,5);
+        //    // verify it got executed
+        //    expect(calc.first).toHaveBeenCalled();
+        //    expect(calc.first).toHaveBeenCalledWith(3,5);
+
             expect(function() {
-                calc.factorial(-7)
+                calc.first(5, 5)
             }).toThrowError(Error);
+       });
+
+       it("should be able to calculate triangle of 3 and 5", function() {
+             //call any method
+             calc.second(3,5,4,6);
+            // verify it got executed
+            expect(calc.second).toHaveBeenCalled();
+            expect(calc.second).toHaveBeenCalledWith(3,5,4,6);
         });
 
-        it("should be able to calculate teste of 3 and 5", function() {
-            //call any method
-            calc.first(3,5);
-           // verify it got executed
-           expect(calc.first).toHaveBeenCalled();
-           expect(calc.first).toHaveBeenCalledWith(3,5);
-       });
+       it("should be able to pass with 90", function() {
+             //call any method
+             calc.second(90);
+            // verify it got executed
+            expect(calc.second).toHaveBeenCalled();
+            expect(calc.second).toHaveBeenCalledWith(90);
+        });
          
     });
 });
--- src/MathUtils.js
+++ src/MathUtils.js
@@ -33,23 +33,62 @@ MathUtils.prototype.factorial = function(number) {
 
 MathUtils.prototype.first = function(number1, number2) {
     // Check if is ímpar
-    if(number1/2 == 1 || number2/2 == 1){    
+    if(number1 & 1 || number2 & 1){    
         let resultado = number1 * number2;
         if(resultado > 10) {
-            console.log(resultado)
+            throw new Error(resultado + " 2 verdadeiros")
         }else {
             resultado = resultado + 10;
-            console.log(resultado);
+            throw new Error(resultado + " 1 verdadeiro, 1 falso");
         }
     }else {
         resultado = number1 + number2;
 
         if(resultado > 5) {
             resultado = resultado + 5;
-            console.log(resultado)
+            throw new Error(resultado + " 1 falso, 1 verdadeiro");
         }else {
-            console.log(resultado)
+            throw new Error(resultado + " 2 falsos");
         }
     }
 }
 
+MathUtils.prototype.second = function(lados, lado1, lado2, lado3) {
+    if(lados == 3) {
+        let tipo = (lado1 === lado2 && lado2 === lado3) && 'equilateral' ||
+        (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) && 'isosceles' ||
+        'scalene';
+        console.log("É triangulo")
+        let perimetro = lado1 + lado2 + lado3;
+        console.log(perimetro);
+    }else {
+        if(lados == 4) {
+            if(lado1 == lado2 == lado3) {
+                console.log("É quadrado");
+                let perimetro = lado1 + lado2 + lado3;
+                console.log(perimetro);
+            }else {
+                console.log("Tem quatro lados");
+                let perimetro = lado1 + lado2 + lado3;
+                console.log(perimetro);
+            }
+        }else {
+            console.log("Tem " + lados + " lados")
+            let perimetro = lado1 + lado2 + lado3;
+            console.log(perimetro);
+        }
+    }
+}
+
+MathUtils.prototype.third = function(angulo) {
+    if(angulo > 90) {
+        angulo = Math.sin(angulo);
+        console.log(angulo);
+    }else {
+        angulo = Math.cos(angulo);
+        console.log(angulo);
+    }
+    if(Math.tan(angulo) < 1 || Math.tan(angulo) > 1) {
+        console.log(Math.tan(angulo))
+    }
+}
